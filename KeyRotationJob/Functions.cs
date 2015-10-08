aae898d26b09bdb548ad2f95e5c7ebd3a9efe45f
Andrei Dzimchuk <andrei@dzimchuk.net>
2015-10-08 12:34:07 +0300

    added SAS key rotation job

---
+++ KeyRotationJob/Functions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace KeyRotationJob
+{
+    public class Functions
+    {
+        public static void RegenerateKey(TextWriter log)
+        {
+            var manager = NamespaceManager.CreateFromConnectionString(ConfigurationManager.AppSettings["ServiceBusConnectionString"]);
+            var description = manager.GetQueue(ConfigurationManager.AppSettings["QueueName"]);
+
+            RegenerateKey(description, ConfigurationManager.AppSettings["ReadAuthorizationRuleName"], log);
+            RegenerateKey(description, ConfigurationManager.AppSettings["WriteAuthorizationRuleName"], log);
+
+            manager.UpdateQueue(description);
+        }
+
+        private static void RegenerateKey(QueueDescription description, string ruleName, TextWriter log)
+        {
+            SharedAccessAuthorizationRule rule;
+            if (!description.Authorization.TryGetSharedAccessAuthorizationRule(ruleName, out rule))
+                throw new Exception($"Authorization rule {ruleName} was not found");
+
+            rule.SecondaryKey = rule.PrimaryKey;
+            rule.PrimaryKey = SharedAccessAuthorizationRule.GenerateRandomKey();
+
+            log.WriteLine($"Authorization rule: {ruleName}\nPrimary key: {rule.PrimaryKey}\nSecondary key: {rule.SecondaryKey}");
+        }
+    }
+}
---
+++ KeyRotationJob/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.WebJobs;
+
+namespace KeyRotationJob
+{
+    class Program
+    {
+        static void Main()
+        {
+            var host = new JobHost();
+            host.Call(typeof(Functions).GetMethod("RegenerateKey"));
+        }
+    }
+}
