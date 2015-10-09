99a4df3c5c3507c7358e81c9eadb85c00c5820d1
Andrei Dzimchuk <andrei@dzimchuk.net>
2015-10-09 12:48:34 +0300

    added Sender and Listener

---
+++ Listener/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json.Linq;
+
+namespace Listener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Ctrl+C to exit.");
+            ReceiveMessages().Wait();
+        }
+
+        private static async Task ReceiveMessages()
+        {
+            var client = await GetQueueClientAsync();
+            while (true)
+            {
+                try
+                {
+                    var message = await client.ReceiveAsync(TimeSpan.MaxValue);
+                    await message.CompleteAsync();
+                    Console.WriteLine("{0} Received {1}", DateTime.Now, message.GetBody<Guid>());
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Console.WriteLine(e.Message);
+                    client = await GetQueueClientAsync();
+                }
+            }
+        }
+
+        private static async Task<QueueClient> GetQueueClientAsync()
+        {
+            var sharedAccessSignature = await GetTokenAsync();
+
+            var address = ServiceBusEnvironment.CreateServiceUri("sb", ConfigurationManager.AppSettings["Namespace"], string.Empty);
+            var messagingFactory = MessagingFactory.Create(address, TokenProvider.CreateSharedAccessSignatureTokenProvider(sharedAccessSignature));
+            return messagingFactory.CreateQueueClient(ConfigurationManager.AppSettings["QueueName"]);
+        }
+
+        private static async Task<string> GetTokenAsync()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await client.GetStringAsync(ConfigurationManager.AppSettings["ReadTokenUrl"]);
+            var jObject = JObject.Parse(response);
+            return jObject.GetValue("SharedAccessSignature").ToString();
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json.Linq;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Ctrl+C to exit.");
+            SendMessages().Wait();
+        }
+
+        private static async Task SendMessages()
+        {
+            var client = await GetQueueClientAsync();
+            while (true)
+            {
+                try
+                {
+                    var message = new BrokeredMessage(Guid.NewGuid());
+                    await client.SendAsync(message);
+
+                    Console.WriteLine("{0} Sent {1}", DateTime.Now, message.GetBody<Guid>());
+                }
+                catch(UnauthorizedAccessException e)
+                {
+                    Console.WriteLine(e.Message);
+                    client = await GetQueueClientAsync();
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(8));
+            }
+        }
+
+        private static async Task<QueueClient> GetQueueClientAsync()
+        {
+            var sharedAccessSignature = await GetTokenAsync();
+
+            var address = ServiceBusEnvironment.CreateServiceUri("sb", ConfigurationManager.AppSettings["Namespace"], string.Empty);
+            var messagingFactory = MessagingFactory.Create(address, TokenProvider.CreateSharedAccessSignatureTokenProvider(sharedAccessSignature));
+            return messagingFactory.CreateQueueClient(ConfigurationManager.AppSettings["QueueName"]);
+        }
+
+        private static async Task<string> GetTokenAsync()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await client.GetStringAsync(ConfigurationManager.AppSettings["WriteTokenUrl"]);
+            var jObject = JObject.Parse(response);
+            return jObject.GetValue("SharedAccessSignature").ToString();
+        }
+    }
+}
